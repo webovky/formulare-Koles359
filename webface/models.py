6554f2645f38df6046347ff73bc634797583d317
Koles359 <71071638+Koles359@users.noreply.github.com>
2020-12-17 14:12:27 +0100

    Add files via upload

--- webface/models.py
+++ webface/models.py
@@ -1,5 +1,5 @@
 # from datetime import datetime
-from pony.orm import PrimaryKey, Required, Optional, Database  # , Set
+from pony.orm import PrimaryKey, Required, Optional, Database , Set
 
 
 db = Database()
@@ -9,8 +9,15 @@ db.bind(provider="sqlite", filename="./database.sqlite", create_db=True)
 class Uzivatel(db.Entity):
     id = PrimaryKey(int, auto=True)
     login = Required(str)
-    password = Required(str)
+    password = Optional(str)
     email = Optional(str)
+    zkracovace=Set('Zkracovac')
+
+class Zkracovac(db.Entity):
+    zkratka = PrimaryKey(str)
+    url = Required(str)
+    uzivatel=Optional(Uzivatel)
+
 
 
 db.generate_mapping(create_tables=True)
